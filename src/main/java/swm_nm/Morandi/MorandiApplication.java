6478589ce30481b282ca8c9453aac314f8fc1bee
aj4941@naver.com <aj4941@naver.com>
2023-09-13 01:19:27 +0900

    init commit

---
+++ src/main/java/swm_nm/Morandi/MorandiApplication.java
@@ -0,0 +1,14 @@
+package swm_nm.Morandi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorandiApplication {
+
+	// Morandi-compiler test
+	public static void main(String[] args) {
+		SpringApplication.run(MorandiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/swm_nm/Morandi/MorandiApplicationTests.java
@@ -0,0 +1,13 @@
+package swm_nm.Morandi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MorandiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
