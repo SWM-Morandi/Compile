6f210c932b67ce768f1084662ab382ed5a5dd09b
aj4941@naver.com <aj4941@naver.com>
2023-10-06 15:45:07 +0900

    :recycle: [REFACTOR] 테스트케이스 결과 반환 / 코드 실행 결과 반환 분리

--- src/main/java/swm_nm/morandi/domain/compile/controller/CompileController.java
+++ src/main/java/swm_nm/morandi/domain/compile/controller/CompileController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import swm_nm.morandi.domain.compile.dto.InputDto;
 import swm_nm.morandi.domain.compile.dto.OutputDto;
+import swm_nm.morandi.domain.compile.dto.TestCaseInputDto;
 import swm_nm.morandi.domain.compile.service.CompileService;
 
 import java.util.List;
@@ -22,8 +23,13 @@ public class CompileController {
 
     private final CompileService compileService;
     @PostMapping
-    public ResponseEntity<List<OutputDto>> getCompileResult(@RequestBody InputDto inputDto) {
-        List<OutputDto> outputDto = compileService.compile(inputDto);
+    public ResponseEntity<OutputDto> getCompileResult(@RequestBody InputDto inputDto) {
+        OutputDto outputDto = compileService.compile(inputDto);
+        return new ResponseEntity<>(outputDto, OK);
+    }
+    @PostMapping("/tc")
+    public ResponseEntity<List<OutputDto>> getCompileResult(@RequestBody TestCaseInputDto testCaseInputDto) {
+        List<OutputDto> outputDto = compileService.testCaseCompile(testCaseInputDto);
         return new ResponseEntity<>(outputDto, OK);
     }
 }
